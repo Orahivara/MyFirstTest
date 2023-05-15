c39c3019dac3a2b6e06bfddb0baf76ba09cd7145
Shitikova Anastasia <kiomi689oosava@mail.com>
2023-05-15 23:07:27 +0300

    Добавьте файлы проекта.

---
+++ MyFirstTest/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyFirstTest
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+          
+            MessageBox.Show(WeekOfMonth( new DateTime (100000, 5, 11), 200).ToString());
+        }
+
+            public static double WeekOfMonth(DateTime date, double a)
+        {
+            try
+            {
+                DateTime start = new DateTime(date.Year, date.Month, 1);
+                while (date.Date.AddDays(1).DayOfWeek != CultureInfo.CurrentCulture.DateTimeFormat.FirstDayOfWeek)
+                    date = date.AddDays(1);
+                int weekinmouth = (int)Math.Truncate((double)date.Subtract(start).TotalDays / 7f) + 1;
+                if (weekinmouth == 4 &&a>0)
+                {
+                    return a - (a / 100 * 4);
+                }
+                if (a <= 0)
+                {
+                    return 0;
+                }
+                return a;
+                
+                
+                            }
+            catch 
+            {
+
+                MessageBox.Show("Неккоректные данные") ;
+                return 0;
+            }
+              
+            }
+        }
+    }
+
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MyFirstTest;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double start = 1000;
+            double c = 960;
+            DateTime A1 = new DateTime(2015, 7, 20);
+                Assert.AreEqual(c,Convert.ToDouble(MyFirstTest.MainWindow.WeekOfMonth(A1, start)));
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            double c = 1000;
+            DateTime A1 = new DateTime(2015, 7, 1);
+            Assert.AreEqual(c, Convert.ToDouble(MyFirstTest.MainWindow.WeekOfMonth(A1, c)));
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            double c = -960;
+            DateTime A1 = new DateTime(2015, 7, 1);
+            Assert.AreEqual(0,MyFirstTest.MainWindow.WeekOfMonth(A1, c));
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            double c = 0;
+            DateTime A1 = new DateTime(2015, 7, 20);
+            Assert.AreEqual(0, MyFirstTest.MainWindow.WeekOfMonth(A1, c));
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            double start = (int.MaxValue - (int.MaxValue/100*4));
+            double c =int.MaxValue ;
+            DateTime A1 = new DateTime(2015, 7, 20);
+            Assert.AreEqual(start,MyFirstTest.MainWindow.WeekOfMonth(A1, c));
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+            double start= (double.MaxValue - (double.MaxValue/100*4));
+            double c = double.MaxValue;
+            DateTime A1 = new DateTime(2015, 7, 20);
+            Assert.AreEqual(start, Convert.ToDouble(MyFirstTest.MainWindow.WeekOfMonth(A1, c)));
+        }
+        [TestMethod]
+        public void TestMethod7()
+        {
+            try
+            {
+                double c = 960;
+                DateTime A1 = new DateTime(-100, 1, 1);
+                Assert.IsNotNull(MyFirstTest.MainWindow.WeekOfMonth(A1, c));
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+
+                Assert.Inconclusive("Неверное значение");
+            }
+          
+        }
+        [TestMethod]
+        public void TestMethod8()
+        {
+            try
+            {
+                double c = 960;
+                DateTime A1 = new DateTime(0, 1, 1);
+                Assert.IsNotNull(MyFirstTest.MainWindow.WeekOfMonth(A1, c));
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Assert.Inconclusive("Неверное значение");
+            }
+            
+        }
+        [TestMethod]
+        public void TestMethod9()
+        {
+
+            double c = 1000;
+            double start = 960;
+            DateTime A1 = new DateTime(2077, 5, 11);
+            // Basket.WeekOfMonth(DateTime.Now, 1000);
+            Assert.AreEqual(start,MyFirstTest.MainWindow.WeekOfMonth(A1, c));
+        }
+        [TestMethod]
+        public void TestMethod10()
+        {
+            double c = long.MaxValue;
+            double start = long.MaxValue - (long.MaxValue / 100 * 4);
+            DateTime A1 = new DateTime(2015, 7, 20);
+            // Basket.WeekOfMonth(DateTime.Now, 1000);
+            Assert.AreEqual(start,MyFirstTest.MainWindow.WeekOfMonth(A1, c));
+        }
+    }
+}
